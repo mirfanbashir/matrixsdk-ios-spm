6cc66043bc4e4b1e8019e0146115b2e3235d7635
Irfan Bashir <irfan4@gmail.com>
2022-11-28 20:58:31 +0100

    Fixed repo name

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "MatrixSDK", targets: ["MatrixSDK"])
     ],
     targets: [
-        .binaryTarget(name: "MatrixSDK", url: "https://github.com/mirfanbashir/MatrixSDK/releases/download/v0.24.3/MatrixSDK.xcframework.zip", checksum: "f70ccf8a960c806aae649f2a313fb31dcbddf87efd2e1fce811630439826f7f4")
+        .binaryTarget(name: "MatrixSDK", url: "https://github.com/mirfanbashir/matrixsdk-ios-spm/releases/download/v0.24.3/MatrixSDK.xcframework.zip", checksum: "f70ccf8a960c806aae649f2a313fb31dcbddf87efd2e1fce811630439826f7f4")
     ]
 )
